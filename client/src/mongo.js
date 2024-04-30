2993d97660b54ecccbe3275389bb0e91fe9d5a7b
delirious9 <101205666+delirious9@users.noreply.github.com>
2024-04-30 05:40:20 +0100

    Update mongo.js

--- client/src/mongo.js
+++ client/src/mongo.js
@@ -1,16 +1,10 @@
-// Code  for mongoose config in backend
-// Filename - backend/index.js
- 
-// To connect with your mongoDB database
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/', {
     dbName: 'watchApp',
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, err => err ? console.log(err) : 
     console.log('Connected to watchApp database'));
- 
-// Schema for users of app
+
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -33,11 +27,54 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+    // Adding User ID component
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 });
+
+// WatchCard model
+const WatchCardSchema = new mongoose.Schema({
+    brand: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    condition: {
+        type: String,
+        required: true
+    }
+});
+
+const PortfolioSchema = new mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    // List of WatchCard models
+    watches: [WatchCardSchema]
+});
+
+const WatchCard = mongoose.model('WatchCard', WatchCardSchema);
+const Portfolio = mongoose.model('Portfolio', PortfolioSchema);
+
 const User = mongoose.model('users', UserSchema);
 User.createIndexes();
- 
-// For backend and express
+
 const express = require('express');
 const app = express();
 const cors = require("cors");
@@ -45,10 +82,9 @@ console.log("App listen at port 5000");
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, resp) => {
- 
     resp.send("App is Working");
 });
- 
+
 app.post("/register", async (req, resp) => {
     try {
         const user = new User(req.body);
@@ -61,9 +97,9 @@ app.post("/register", async (req, resp) => {
         } else {
             console.log("User already register");
         }
- 
     } catch (e) {
         resp.send("Something Went Wrong");
     }
 });
+
 app.listen(5000);
